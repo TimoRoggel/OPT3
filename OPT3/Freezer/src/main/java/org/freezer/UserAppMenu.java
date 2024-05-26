803a59fb2a5bf88e09278679ad09116ebc3f8f7a
Timo Brouwer <timobrouwer@Timos-MacBook-Pro.local>
2024-05-26 13:36:24 +0200

    Updated

--- OPT3/Freezer/src/main/java/org/freezer/UserAppMenu.java
+++ OPT3/Freezer/src/main/java/org/freezer/UserAppMenu.java
@@ -2,24 +2,15 @@ package org.freezer;
 
 import java.util.Scanner;
 
-public class UserAppMenu extends AppMenu {
+public class UserAppMenu {
     private final Scanner scanner = new Scanner(System.in);
     private final Manager manager = new Manager();
     private final PrinterManager printerManager = new PrinterManager();
 
-    @Override
     public void run() {
         while (true) {
-            System.out.println("\nUser Menu:");
-            System.out.println("3. Product Lijst");
-            System.out.println("4. Sluiten");
-            System.out.println("-----------------------");
-            System.out.println("5. Product Lijst Printen");
-            System.out.println("6. Stuur lijst via email");
-            System.out.print("Kies een optie: ");
-
-            int choice = scanner.nextInt();
-            scanner.nextLine();
+            displayUserMenu();
+            int choice = getUserChoice();
             switch (choice) {
                 case 3:
                     manager.displayProductList();
@@ -28,17 +19,39 @@ public class UserAppMenu extends AppMenu {
                     System.out.println("Sluiten...");
                     return;
                 case 5:
-                    System.out.println("Lijst wordt uitgeprint...");
-                    printerManager.printProductList();
+                    printProductList();
                     break;
                 case 6:
-                    System.out.println("Uw email:");
-                    String emailAddress = scanner.nextLine();
-                    manager.sendProductListByEmail(emailAddress);
+                    sendProductListByEmail();
                     break;
                 default:
                     System.out.println("Geen toegangsrechten voor deze optie.");
             }
         }
+    }
+
+    private void displayUserMenu() {
+        System.out.println("\nUser Menu:");
+        System.out.println("3. Product Lijst");
+        System.out.println("4. Sluiten");
+        System.out.println("-----------------------");
+        System.out.println("5. Product Lijst Printen");
+        System.out.println("6. Stuur lijst via email");
+        System.out.print("Kies een optie: ");
+    }
+
+    private int getUserChoice() {
+        return scanner.nextInt();
+    }
+
+    private void printProductList() {
+        System.out.println("Lijst wordt uitgeprint...");
+        printerManager.printProductList();
+    }
+
+    private void sendProductListByEmail() {
+        System.out.println("Uw email:");
+        String emailAddress = scanner.nextLine();
+        manager.sendProductListByEmail(emailAddress);
     }
 }
